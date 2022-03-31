644e0b942889847dc2dd5735d491e1248e108467
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2022-03-31 21:08:36 +0200

    wip

--- root/usr/local/bin/docker-entrypoint.sh
+++ root/usr/local/bin/docker-entrypoint.sh
@@ -9,10 +9,18 @@ MYGID="${APPGID}"
 
 AutoUpgrade
 
-if [ "$1" == 'bridge' ]; then
+if [ "$1" == 'bridge-init' ]; then
+    gpg --generate-key --batch /opt/protonmail/etc/gpg-parameters
+    pass init proton-key
+    /opt/protonmail/bin/proton-bridge --cli $@
+elif [ "$1" == 'bridge' ]; then
   RunDropletEntrypoint
+  DockLog "Bind smtp port 25"
+  socat TCP-LISTEN:25,fork TCP:127.0.0.1:1025 &
+  DockLog "Bind imap port 143"
+  socat TCP-LISTEN:143,fork TCP:127.0.0.1:1143 &
   DockLog "Starting app: ${1}"
-  su-exec ${MYUSER} echo "exited"
+  su-exec ${MYUSER} /opt/protonmail/bin/proton-bridge --cli $@
 else
   DockLog "Starting app: ${@}"
   exec "$@"
