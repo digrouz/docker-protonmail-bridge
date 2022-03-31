cb49c0cd752d4031cdd2b3fa09d85e0a5eb90234
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2022-03-31 16:54:28 +0000

    wip

---
+++ .github/scripts/auto-update.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+BRIDGE_URL="https://api.github.com/repos/ProtonMail/proton-bridge/tags"
+
+LAST_VERSION=$(curl -SsL ${BRIDGE_URL} | \
+               jq -r -c '.[] | select( .name | (contains("rc") | not) and (contains("beta") | not) and (contains("alpha") | not)) | .name' |\
+                head -1 \ 
+              )
+
+sed -i -e "s|BRIDGE_VERSION='.*'|BRIDGE_VERSION='${LAST_VERSION}'|" Dockerfile*
+
+if output=$(git status --porcelain) && [ -z "$output" ]; then
+  # Working directory clean
+  echo "No new version available!"
+else 
+  # Uncommitted changes
+  git commit -a -m "update to version: ${LAST_VERSION}"
+  git push
+fi
