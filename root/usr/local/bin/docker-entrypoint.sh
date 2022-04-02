ef4e22fbf4cdfbfc1e3e2acc51a20988872ca7d8
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2022-04-02 14:35:54 +0200

    fix entrypoint

--- root/usr/local/bin/docker-entrypoint.sh
+++ root/usr/local/bin/docker-entrypoint.sh
@@ -15,24 +15,28 @@ if [ "$1" == 'bridge-init' ]; then
   su-exec ${MYUSER} gpg --generate-key --batch /opt/protonmail/etc/gpg-parameters
   DockLog "Init password manager"
   su-exec ${MYUSER} pass init proton-key
+  
   DockLog "Init Bridge"
   exec su-exec ${MYUSER} /opt/protonmail/bin/proton-bridge --cli init
 elif [ "$1" == 'bridge' ]; then
   RunDropletEntrypoint
+  
   DockLog "Bind smtp port 25"
   socat TCP-LISTEN:25,fork TCP:127.0.0.1:1025 &
   DockLog "Bind imap port 143"
   socat TCP-LISTEN:143,fork TCP:127.0.0.1:1143 &
+  
   if [ -e /opt/protonmail/etc/protontty ];then
     DockLog "Cleanup TTY /opt/protonmail/etc/protontty"
     rm -rf /opt/protonmail/etc/protontty
-  else
-    DockLog "Cleanup TTY /opt/protonmail/etc/protontty"
-    mkfifo /opt/protonmail/etc/protontty
-    chown ${MYUSER} /opt/protonmail/etc/protontty
   fi
+  
+  DockLog "Creating TTY /opt/protonmail/etc/protontty"
+  mkfifo /opt/protonmail/etc/protontty
+  chown ${MYUSER} /opt/protonmail/etc/protontty
+  
   DockLog "Starting app: ${1}"
-  exec cat /protonmail/etc/protontty | su-exec ${MYUSER} /opt/protonmail/bin/proton-bridge --cli
+  exec cat /opt/protonmail/etc/protontty | su-exec ${MYUSER} /opt/protonmail/bin/proton-bridge --cli
 else
   DockLog "Starting app: ${@}"
   exec "$@"
